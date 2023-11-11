f7319489639279e6ce8e748365d2f425537ee162
Hamza Amar <rhamzax@gmail.com>
2023-11-11 13:31:36 -0700

    Create a nav bar

--- app/src/routes/Header.svelte
+++ app/src/routes/Header.svelte
@@ -1,73 +1,42 @@
 <script>
 	import { page } from '$app/stores';
 	import logo from '$lib/images/svelte-logo.svg';
-	import github from '$lib/images/github.svg';
 </script>
 
-<header>
-	<div class="corner">
-		<a href="https://kit.svelte.dev">
+<header class="flex justify-between items-center px-12 h-24 sticky">
+	<div class="flex justify-center items-center">
+		<h1 class="">Spout</h1>
+		<a href="/">
 			<img src={logo} alt="SvelteKit" />
 		</a>
 	</div>
 
-	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
+	<nav class="flex items-center justify-between md:order-none text-xl ">
+		<!-- <svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
+		</svg> -->
 		<ul>
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
+				<a href="/">My Home</a>
 			</li>
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
+				<a href="/about">Connect</a>
 			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
+			<li aria-current={$page.url.pathname === '/sverdle' ? 'page' : undefined}>
+				<a href="/sverdle">Support</a>
 			</li>
 		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
+		<!-- <svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
+		</svg> -->
 	</nav>
-
-	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
-			<img src={github} alt="GitHub" />
-		</a>
-	</div>
+	<a href="/login" class="bg-blue-400 h-12 w-36 rounded-md text-white flex justify-center items-center">
+		Login
+	</a>
 </header>
 
+
 <style>
-	header {
-		display: flex;
-		justify-content: space-between;
-	}
-
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
-	}
-
-	nav {
-		display: flex;
-		justify-content: center;
-		--background: rgba(255, 255, 255, 0.7);
-	}
 
 	svg {
 		width: 2em;
@@ -75,17 +44,14 @@
 		display: block;
 	}
 
-	path {
-		fill: var(--background);
-	}
-
 	ul {
 		position: relative;
 		padding: 0;
 		margin: 0;
 		height: 3em;
+		width: 15em;
 		display: flex;
-		justify-content: center;
+		justify-content: space-between;
 		align-items: center;
 		list-style: none;
 		background: var(--background);
