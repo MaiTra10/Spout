fd31e177f93b3ace3d5646536d657a249d1efdb1
Maitra Patel <10maitra22@gmail.com>
2023-11-12 00:02:05 -0700

    feat
    All basic functionalities work

--- API/test_sprinkler.py
+++ API/test_sprinkler.py
@@ -11,30 +11,58 @@ print(s.recv(1024).decode())
 s.close() """
 
 import requests
-import json
-
 import time
 
-data = {'function': 'start', 'id': 0}
+print(' ')
+
+data = {'function': 'add', 'id': 3, 'name': 'Back Yard Flower Sprinkler', 'period': '4', 'seed_type': 3}
 
 r1 = requests.post('http://172.18.144.1:12000', data)
 
-time.sleep(17)
+print(r1.content.decode('utf-8'))
 
-data = {'function': 'stop', 'id': 0}
+r1 = requests.get('http://172.18.144.1:12000')
 
-r1 = requests.post('http://172.18.144.1:12000', data)
+print(r1.content.decode('utf-8'))
 
-""" data = {'function': 'add', 'id': 3, 'name': 'Back Yard Flower Sprinkler', 'period': '4', 'seed_type': 3}
-
-r1 = requests.post('http://172.18.144.1:12000', data)
-
-data = {'function': 'add', 'id': 1, 'name': 'Back Yard Flower Sprinkler', 'period': '4', 'seed_type': 3}
+data = {'function': 'add', 'id': 1, 'name': 'Front Yard Flower Sprinkler', 'period': '2', 'seed_type': 2}
 
 r2 = requests.post('http://172.18.144.1:12000', data)
 
-data = {'id': '1'}
+print(r2.content.decode('utf-8'))
 
-r = requests.delete('http://172.18.144.1:12000', headers=data) """
+data = {'function': 'start', 'id': 1}
 
-print(r1)
+r1 = requests.post('http://172.18.144.1:12000', data)
+
+data = {'function': 'start', 'id': 3}
+
+r2 = requests.post('http://172.18.144.1:12000', data)
+
+time.sleep(5)
+
+data = {'function': 'stop', 'id': 1}
+
+r1 = requests.post('http://172.18.144.1:12000', data)
+
+print(r1.content.decode('utf-8'))
+
+time.sleep(12)
+
+data = {'function': 'stop', 'id': 3}
+
+r1 = requests.post('http://172.18.144.1:12000', data)
+
+print(r1.content.decode('utf-8'))
+
+data = {'id': 3}
+
+r1 = requests.delete('http://172.18.144.1:12000', params=data)
+
+print(r1.content.decode('utf-8'))
+
+data = {'id': 1, 'seed_type': 5}
+
+r1 = requests.put('http://172.18.144.1:12000', data)
+
+print(r1.content.decode('utf-8'))
