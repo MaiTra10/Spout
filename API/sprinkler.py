75c3305a18ed06f5ee9d5652bf97fc5429ab63f3
Maitra Patel <10maitra22@gmail.com>
2023-11-12 11:30:40 -0700

    msc

--- API/sprinkler.py
+++ API/sprinkler.py
@@ -80,7 +80,7 @@ def post_handler(c, function, data):
 
             'status': 200,
             'msg': 'OK',
-            'content': str(sprinklers[-1])
+            'content':f'Sprinkler with ID {sid} named {name} has been successfully added! {str(sprinklers[-1])}'
 
         }
 
